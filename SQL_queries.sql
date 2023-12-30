07fecbff705015f65a532c4ee1c62382dc018d7e
fmammo <46895783+fmammo@users.noreply.github.com>
2023-12-30 16:10:27 +0100

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,9 +1,9 @@
--- Create database
-CREATE DATABASE IF NOT EXISTS walmartSales;
+1. Creating a database
+CREATE DATABASE WalSales;
 
 
--- Create table
-CREATE TABLE IF NOT EXISTS sales(
+2. Creating a table
+CREATE TABLE sales(
 	invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
     branch VARCHAR(5) NOT NULL,
     city VARCHAR(30) NOT NULL,
@@ -23,28 +23,20 @@ CREATE TABLE IF NOT EXISTS sales(
     rating FLOAT(2, 1)
 );
 
--- Data cleaning
+3. Data werangling / cleaning
+	3.1. Visualize the available dataset
 SELECT
 	*
 FROM sales;
 
+4. Feature engineering
+	4.1. Add the time_of_day column
 
--- Add the time_of_day column
-SELECT
-	time,
-	(CASE
-		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
-        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
-        ELSE "Evening"
-    END) AS time_of_day
-FROM sales;
-
-
+-- If below code doesn't work, turn off safe mode for update
+ Go to Edit > Preferences > SQL Edito > scroll down and toggle safe mode
+ Then Reconnect to MySQL: Query > Reconnect to server
+	
 ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
-
--- For this to work turn off safe mode for update
--- Edit > Preferences > SQL Edito > scroll down and toggle safe mode
--- Reconnect to MySQL: Query > Reconnect to server
 UPDATE sales
 SET time_of_day = (
 	CASE
@@ -55,11 +47,7 @@ SET time_of_day = (
 );
 
 
--- Add day_name column
-SELECT
-	date,
-	DAYNAME(date)
-FROM sales;
+4.2. Add day_name column
 
 ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
 
@@ -67,42 +55,35 @@ UPDATE sales
 SET day_name = DAYNAME(date);
 
 
--- Add month_name column
-SELECT
-	date,
-	MONTHNAME(date)
-FROM sales;
+4.3. Add month_name column
 
 ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
 
 UPDATE sales
 SET month_name = MONTHNAME(date);
 
--- --------------------------------------------------------------------
--- ---------------------------- Generic ------------------------------
--- --------------------------------------------------------------------
--- How many unique cities does the data have?
+5. General/Generic EDA
+	
+5.1. How many unique cities does the data have?
 SELECT 
 	DISTINCT city
 FROM sales;
 
--- In which city is each branch?
+
+5.2. In which city is each branch?
 SELECT 
 	DISTINCT city,
     branch
 FROM sales;
 
--- --------------------------------------------------------------------
--- ---------------------------- Product -------------------------------
--- --------------------------------------------------------------------
+6. Product EDA
 
--- How many unique product lines does the data have?
+6.1. How many unique product lines does the data have?
 SELECT
 	DISTINCT product_line
 FROM sales;
 
-
--- What is the most selling product line
+6.2. What is the most selling product line
 SELECT
 	SUM(quantity) as qty,
     product_line
@@ -110,7 +91,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
--- What is the most selling product line
+6.3. What is the most selling product line
 SELECT
 	SUM(quantity) as qty,
     product_line
@@ -118,7 +99,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
--- What is the total revenue by month
+6.4. What is the total revenue by month
 SELECT
 	month_name AS month,
 	SUM(total) AS total_revenue
@@ -126,8 +107,7 @@ FROM sales
 GROUP BY month_name 
 ORDER BY total_revenue;
 
-
--- What month had the largest COGS?
+6.5. What month had the largest COGS(Cost Of Good Sold)?
 SELECT
 	month_name AS month,
 	SUM(cogs) AS cogs
@@ -135,8 +115,7 @@ FROM sales
 GROUP BY month_name 
 ORDER BY cogs;
 
-
--- What product line had the largest revenue?
+6.6. What product line had the largest revenue?
 SELECT
 	product_line,
 	SUM(total) as total_revenue
@@ -144,7 +123,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY total_revenue DESC;
 
--- What is the city with the largest revenue?
+6.7. What is the city with the largest revenue?
 SELECT
 	branch,
 	city,
@@ -153,8 +132,7 @@ FROM sales
 GROUP BY city, branch 
 ORDER BY total_revenue;
 
-
--- What product line had the largest VAT?
+6.8. What product line had the largest VAT?
 SELECT
 	product_line,
 	AVG(tax_pct) as avg_tax
@@ -163,8 +141,8 @@ GROUP BY product_line
 ORDER BY avg_tax DESC;
 
 
--- Fetch each product line and add a column to those product 
--- line showing "Good", "Bad". Good if its greater than average sales
+6.9. Fetch each product line and add a column to those product line showing "Good"/ "Bad". 
+   Good if its greater than average sales
 
 SELECT 
 	AVG(quantity) AS avg_qnty
@@ -180,7 +158,7 @@ FROM sales
 GROUP BY product_line;
 
 
--- Which branch sold more products than average product sold?
+6.10. Which branch sold more products than average product sold?
 SELECT 
 	branch, 
     SUM(quantity) AS qnty
@@ -189,7 +167,7 @@ GROUP BY branch
 HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
 
 
--- What is the most common product line by gender
+6.11. What is the most common product line by gender
 SELECT
 	gender,
     product_line,
@@ -198,7 +176,7 @@ FROM sales
 GROUP BY gender, product_line
 ORDER BY total_cnt DESC;
 
--- What is the average rating of each product line
+6.12. What is the average rating of each product line
 SELECT
 	ROUND(AVG(rating), 2) as avg_rating,
     product_line
@@ -206,25 +184,56 @@ FROM sales
 GROUP BY product_line
 ORDER BY avg_rating DESC;
 
--- --------------------------------------------------------------------
--- --------------------------------------------------------------------
 
--- --------------------------------------------------------------------
--- -------------------------- Customers -------------------------------
--- --------------------------------------------------------------------
+7. Sales EDA
+
+7.1. Number of sales made in each time of the day per weekday 
+SELECT
+	time_of_day,
+	COUNT(*) AS total_sales
+FROM sales
+WHERE day_name = "Sunday"
+GROUP BY time_of_day 
+ORDER BY total_sales DESC;
+-- Evenings experience of most sales, Stores are filled during the evening hours
+
+7.2. Which of the customer types brings the most revenue?
+SELECT
+	customer_type,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY customer_type
+ORDER BY total_revenue;
+
+7.3. Which city has the largest tax/VAT percent?
+SELECT
+	city,
+    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
+FROM sales
+GROUP BY city 
+ORDER BY avg_tax_pct DESC;
+
+7.4. Which customer type pays the most in VAT?
+SELECT
+	customer_type,
+	AVG(tax_pct) AS total_tax
+FROM sales
+GROUP BY customer_type
+ORDER BY total_tax;
+
+8. Customer EDA
 
 -- How many unique customer types does the data have?
 SELECT
 	DISTINCT customer_type
 FROM sales;
 
--- How many unique payment methods does the data have?
+8.1. How many unique payment methods does the data have?
 SELECT
 	DISTINCT payment
 FROM sales;
 
-
--- What is the most common customer type?
+8.2. What is the most common customer type?
 SELECT
 	customer_type,
 	count(*) as count
@@ -232,15 +241,14 @@ FROM sales
 GROUP BY customer_type
 ORDER BY count DESC;
 
--- Which customer type buys the most?
+8.3. Which customer type buys the most?
 SELECT
 	customer_type,
     COUNT(*)
 FROM sales
 GROUP BY customer_type;
 
-
--- What is the gender of most of the customers?
+8.4. What is the gender of most of the customers?
 SELECT
 	gender,
 	COUNT(*) as gender_cnt
@@ -248,7 +256,7 @@ FROM sales
 GROUP BY gender
 ORDER BY gender_cnt DESC;
 
--- What is the gender distribution per branch?
+8.5. What is the gender distribution per branch?
 SELECT
 	gender,
 	COUNT(*) as gender_cnt
@@ -256,21 +264,18 @@ FROM sales
 WHERE branch = "C"
 GROUP BY gender
 ORDER BY gender_cnt DESC;
--- Gender per branch is more or less the same hence, I don't think has
--- an effect of the sales per branch and other factors.
+-- Gender per branch seems almost the same therefore, I don't think has an effect like the sales per branch and other factors.
 
--- Which time of the day do customers give most ratings?
+8.6. Which time of the day do customers give most ratings?
 SELECT
 	time_of_day,
 	AVG(rating) AS avg_rating
 FROM sales
 GROUP BY time_of_day
 ORDER BY avg_rating DESC;
--- Looks like time of the day does not really affect the rating, its
--- more or less the same rating each time of the day.alter
+-- Seems like time of the day doesn't affect the rating, its almost the same rating each time of the day
 
-
--- Which time of the day do customers give most ratings per branch?
+8.7. Which time of the day do customers give most ratings per branch?
 SELECT
 	time_of_day,
 	AVG(rating) AS avg_rating
@@ -278,23 +283,18 @@ FROM sales
 WHERE branch = "A"
 GROUP BY time_of_day
 ORDER BY avg_rating DESC;
--- Branch A and C are doing well in ratings, branch B needs to do a 
--- little more to get better ratings.
+-- Branch A and C are doing well in ratings, branch B needs to do more to get improved ratings.
 
-
--- Which day fo the week has the best avg ratings?
+8.8. Which day of the week has the best avg ratings?
 SELECT
 	day_name,
 	AVG(rating) AS avg_rating
 FROM sales
 GROUP BY day_name 
 ORDER BY avg_rating DESC;
--- Mon, Tue and Friday are the top best days for good ratings
--- why is that the case, how many sales are made on these days?
+-- Mon, Tue and Friday are the top best days for good ratings; why it is like this, how many sales are made on these days?
 
-
-
--- Which day of the week has the best average ratings per branch?
+8.9. Which day of the week has the best average ratings per branch?
 SELECT 
 	day_name,
 	COUNT(day_name) total_sales
@@ -303,48 +303,3 @@ WHERE branch = "C"
 GROUP BY day_name
 ORDER BY total_sales DESC;
 
-
--- --------------------------------------------------------------------
--- --------------------------------------------------------------------
-
--- --------------------------------------------------------------------
--- ---------------------------- Sales ---------------------------------
--- --------------------------------------------------------------------
-
--- Number of sales made in each time of the day per weekday 
-SELECT
-	time_of_day,
-	COUNT(*) AS total_sales
-FROM sales
-WHERE day_name = "Sunday"
-GROUP BY time_of_day 
-ORDER BY total_sales DESC;
--- Evenings experience most sales, the stores are 
--- filled during the evening hours
-
--- Which of the customer types brings the most revenue?
-SELECT
-	customer_type,
-	SUM(total) AS total_revenue
-FROM sales
-GROUP BY customer_type
-ORDER BY total_revenue;
-
--- Which city has the largest tax/VAT percent?
-SELECT
-	city,
-    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
-FROM sales
-GROUP BY city 
-ORDER BY avg_tax_pct DESC;
-
--- Which customer type pays the most in VAT?
-SELECT
-	customer_type,
-	AVG(tax_pct) AS total_tax
-FROM sales
-GROUP BY customer_type
-ORDER BY total_tax;
-
--- --------------------------------------------------------------------
--- --------------------------------------------------------------------
